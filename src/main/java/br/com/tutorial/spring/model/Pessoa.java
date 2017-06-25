6843c144acb0d505484d6970625e468d14404382
Alexandre Uczak <alexandre.uczak@gmail.com>
2017-06-25 14:48:14 -0300

    Init repo

---
+++ src/main/java/br/com/tutorial/spring/model/Pessoa.java
@@ -0,0 +1,50 @@
+package br.com.tutorial.spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+	@Id
+	@Column(name = "codigo")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer codigo;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "apelido")
+	private String apelido;
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getApelido() {
+		return apelido;
+	}
+	
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+	
+}
